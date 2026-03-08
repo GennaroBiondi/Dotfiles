e469e38e0012d9dcbb7f87de4793d7be0ead91af
Gennaro_Biondi <biondigenny31@gmail.com>
2026-03-08 21:38:43 +0100

    Add system control

---
+++ scripts/.local/bin/system_control.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+notification_format="Disable"
+
+if makoctl mode | grep -qx dnd; then
+    notification_format="Enable"
+fi
+
+option=$(printf "Shutdown\nReboot\nLogout\n$notification_format notifications" | wofi --dmenu)
+if [ "$option" = "Shutdown" ]; then
+    sudo systemctl poweroff
+elif [ "$option" = "Reboot" ]; then
+    sudo systemctl reboot
+elif [ "$option" = "Logout" ]; then
+    hyprctl dispatch exit
+elif [ "$option" = "$notification_format notifications" ]; then
+    . ~/.local/bin/toggle-mako.sh
+fi
