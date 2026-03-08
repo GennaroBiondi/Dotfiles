d1ccd99d3c5f17bdf4932051b3638a04aa6a685c
Gennaro_Biondi <biondigenny31@gmail.com>
2026-03-08 01:25:02 +0100

    Make nvim use global clipboard

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,4 +3,4 @@ vim.opt.relativenumber = true -- Show line numbers relative to current line
 vim.opt.cursorline = true     -- Highlight the cursor current line
 vim.opt.shiftwidth = 4        -- Make tabs 4 chars wide
 
-vim.api.nvim_set_option("clipboard", "unnamed") -- Set clipboard to global
+vim.api.nvim_set_option("clipboard", "unnamedplus") -- Set clipboard to global
