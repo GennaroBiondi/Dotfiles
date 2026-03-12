65ea0b01ac15a90aa72f3fa3bd8d2bc1f0e1e8ed
Gennaro_Biondi <biondigenny31@gmail.com>
2026-03-12 17:44:09 +0100

    Add vimbebetter and toggleterm

---
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        opts = {
+            open_mapping = [[T]],
+            hide_numbers = true,
+            direction = "float",
+            float_opts = {
+                border = "curved",
+            },
+        }
+    }
+}
