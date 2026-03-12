811eb740e6994ba1bb8048e70ee8c593794dc3a9
Gennaro_Biondi <biondigenny31@gmail.com>
2026-03-12 20:49:32 +0100

    Rebind keys nvim

--- nvim/.config/nvim/lua/config/binds.lua
+++ nvim/.config/nvim/lua/config/binds.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>", { desc = "Open lazygit", })
 vim.keymap.set("n", "<leader>lp", ":Lazy<CR>", { desc = "Open lazy.nvim", })
+vim.keymap.set("n", "T", "<cmd>ToggleTerm<CR>", { desc = "Open ToggleTerm"} )
--- nvim/.config/nvim/lua/plugins/toggleterm.lua
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -3,7 +3,6 @@ return {
         "akinsho/toggleterm.nvim",
         version = "*",
         opts = {
-            open_mapping = [[T]],
             hide_numbers = true,
             direction = "float",
             float_opts = {
