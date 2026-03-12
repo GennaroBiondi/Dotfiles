932b84212334ca079a16d9f234672a40f8abd59d
Gennaro_Biondi <biondigenny31@gmail.com>
2026-03-12 17:43:59 +0100

    Rebind keys

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,18 +1,21 @@
 return {
     {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-	    "nvim-lua/plenary.nvim",
-	    "MunifTanjim/nui.nvim",
-	    "nvim-tree/nvim-web-devicons", -- optional, but recommended
-	},
-	lazy = false, -- neo-tree will lazily load itself
-	opts = {
-	    window = {
-		position = "right",
-		width = 25,
-	    }
-	},
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        lazy = false,
+        keys = {
+            { "<C-t>", ":Neotree toggle<CR>", mode = "n" }
+        },
+        opts = {
+            window = {
+                position = "right",
+                width = 25,
+            },
+        }
     }
 }
