ccbc7cb1851fd4e1cbde2de74f33de04311f2fdb
Gennaro_Biondi <biondigenny31@gmail.com>
2026-03-13 20:26:20 +0100

    Fix screenshot control center

--- scripts/.scripts/screenshot.sh
+++ scripts/.scripts/screenshot.sh
@@ -7,16 +7,16 @@ screenshot_to=$(printf " Clipboard\n Filesystem" | wofi --dmenu --sort-ord
 [ -z "$screenshot_to" ] && exit
 
 case "$screenshot_mode" in
-    "Screenshot region")
-        if [ "$screenshot_to" = "Clipboard" ]; then
+    " Screenshot region")
+        if [ "$screenshot_to" = " Clipboard" ]; then
             grim -g "$(slurp)" - | wl-copy
         else
             grim -g "$(slurp)" "$HOME/Temp/screenshot_$(date +%Y%m%d_%H%M%S).png"
         fi
         ;;
-    "Screenshot display")
+    "󰹑 Screenshot display")
 	sleep 1
-        if [ "$screenshot_to" = "Clipboard" ]; then
+        if [ "$screenshot_to" = " Clipboard" ]; then
             grim - | wl-copy
         else
             grim "$HOME/Temp/screenshot_$(date +%Y%m%d_%H%M%S).png"
