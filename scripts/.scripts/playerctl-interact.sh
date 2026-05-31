531484f5eb7649ddd1daccf2dfc7e7a00e53ef6d
Gennaro_Biondi <biondigenny31@gmail.com>
2026-05-31 22:39:31 +0200

    Add the ability to mute the whole system

--- scripts/.scripts/playerctl-interact.sh
+++ scripts/.scripts/playerctl-interact.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env sh
 
-status=$(playerctl status 2>/dev/null)
-
-case "$status" in
+audio_paused=$(playerctl status 2>/dev/null)
+case "$audio_paused" in
     Playing) label=" Pause media" ;;
-    Paused) label=" Play media" ;;
-    *) label=" Play media" ;;
+    Paused)  label=" Play media"  ;;
+    *)       label=" Play media"  ;;
 esac
 
-choice=$(printf '%s\n' "$label" "󰒭 Next track" "󰒮 Previous track" | wofi --dmenu --sort-order alphabetical)
+mute_status=$(pactl get-sink-mute @DEFAULT_SINK@)
+case "$mute_status" in
+    *yes*) mute_label="󰕾 Unmute audio" ;;
+    *)     mute_label="󰖁 Mute audio"   ;;
+esac
+
+choice=$(printf '%s\n' "$label" "󰒭 Next track" "󰒮 Previous track" "$mute_label" | wofi --dmenu --sort-order alphabetical)
 
 if [ "$choice" = "$label" ]; then
     playerctl play-pause
@@ -16,4 +21,6 @@ elif [ "$choice" = "󰒭 Next track" ]; then
     playerctl next
 elif [ "$choice" = "󰒮 Previous track" ]; then
     playerctl previous
+elif [ "$choice" = "$mute_label" ]; then
+    pactl set-sink-mute @DEFAULT_SINK@ toggle
 fi
