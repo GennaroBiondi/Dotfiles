8d1aa68c8bc6284840febda98765165aeb408564
Gennaro_Biondi <biondigenny31@gmail.com>
2026-05-31 12:06:43 +0200

    Make nvim's background transparent

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -9,3 +9,8 @@ vim.api.nvim_set_option("clipboard", "unnamedplus") -- Set clipboard to global
 vim.diagnostic.config({
 	virtual_text = true,
 })
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" }) -- non-current windows
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- floating windows
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" }) -- gutter
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" }) -- the ~ lines
