7b4073c65796a0646d2ffbfd659dc854cf72d333
GennaroBiondi <throwawayishere@ultrakill.com>
2026-06-22 01:08:30 +0200

    Add qutebrowser config

---
+++ qutebrowser/.config/qutebrowser/config.py
@@ -0,0 +1,132 @@
+config.load_autoconfig()
+
+c.auto_save.session = True
+
+lack = "#708090"
+luster = "#deeeed"
+orange = "#ffaa88"
+yellow = "#abab77"
+green = "#789978"
+blue = "#7788AA"
+red = "#D70000"
+
+black = "#000000"
+gray1 = "#080808"
+gray2 = "#191919"
+gray3 = "#2a2a2a"
+gray4 = "#444444"
+gray5 = "#555555"
+gray6 = "#7a7a7a"
+gray7 = "#aaaaaa"
+gray8 = "#cccccc"
+gray9 = "#DDDDDD"
+
+c.fonts.tabs.selected = "12pt default_family"
+c.fonts.tabs.unselected = "12pt default_family"
+c.tabs.padding = {"top": 6, "bottom": 6, "left": 8, "right": 8}
+
+c.colors.webpage.bg = gray1
+
+c.colors.statusbar.normal.fg = gray7
+c.colors.statusbar.normal.bg = gray2
+c.colors.statusbar.command.fg = gray7
+c.colors.statusbar.command.bg = gray2
+c.colors.statusbar.command.private.fg = lack
+c.colors.statusbar.command.private.bg = gray2
+c.colors.statusbar.insert.fg = green
+c.colors.statusbar.insert.bg = gray2
+c.colors.statusbar.passthrough.fg = orange
+c.colors.statusbar.passthrough.bg = gray2
+c.colors.statusbar.caret.fg = yellow
+c.colors.statusbar.caret.bg = gray2
+c.colors.statusbar.caret.selection.fg = yellow
+c.colors.statusbar.caret.selection.bg = gray3
+c.colors.statusbar.private.fg = lack
+c.colors.statusbar.private.bg = gray2
+c.colors.statusbar.progress.bg = blue
+
+c.colors.statusbar.url.fg = gray7
+c.colors.statusbar.url.error.fg = red
+c.colors.statusbar.url.hover.fg = luster
+c.colors.statusbar.url.success.http.fg = gray7
+c.colors.statusbar.url.success.https.fg = green
+c.colors.statusbar.url.warn.fg = yellow
+
+c.colors.tabs.bar.bg = gray2
+c.colors.tabs.even.fg = gray6
+c.colors.tabs.even.bg = gray2
+c.colors.tabs.odd.fg = gray6
+c.colors.tabs.odd.bg = gray2
+c.colors.tabs.selected.even.fg = luster
+c.colors.tabs.selected.even.bg = gray3
+c.colors.tabs.selected.odd.fg = luster
+c.colors.tabs.selected.odd.bg = gray3
+c.colors.tabs.pinned.even.fg = gray7
+c.colors.tabs.pinned.even.bg = gray2
+c.colors.tabs.pinned.odd.fg = gray7
+c.colors.tabs.pinned.odd.bg = gray2
+c.colors.tabs.pinned.selected.even.fg = luster
+c.colors.tabs.pinned.selected.even.bg = gray3
+c.colors.tabs.pinned.selected.odd.fg = luster
+c.colors.tabs.pinned.selected.odd.bg = gray3
+c.colors.tabs.indicator.start = lack
+c.colors.tabs.indicator.stop = blue
+c.colors.tabs.indicator.error = red
+
+c.colors.completion.fg = gray7
+c.colors.completion.odd.bg = gray2
+c.colors.completion.even.bg = gray1
+c.colors.completion.category.fg = lack
+c.colors.completion.category.bg = gray2
+c.colors.completion.category.border.top = gray4
+c.colors.completion.category.border.bottom = gray4
+c.colors.completion.item.selected.fg = luster
+c.colors.completion.item.selected.bg = gray3
+c.colors.completion.item.selected.border.top = gray4
+c.colors.completion.item.selected.border.bottom = gray4
+c.colors.completion.item.selected.match.fg = orange
+c.colors.completion.match.fg = orange
+c.colors.completion.scrollbar.fg = gray6
+c.colors.completion.scrollbar.bg = gray2
+
+c.colors.hints.fg = gray1
+c.colors.hints.bg = lack
+c.colors.hints.match.fg = orange
+
+c.colors.keyhint.fg = gray7
+c.colors.keyhint.bg = gray2
+c.colors.keyhint.suffix.fg = lack
+
+c.colors.downloads.bar.bg = gray2
+c.colors.downloads.start.fg = gray1
+c.colors.downloads.start.bg = green
+c.colors.downloads.stop.fg = gray1
+c.colors.downloads.stop.bg = lack
+c.colors.downloads.error.fg = red
+c.colors.downloads.error.bg = gray2
+
+c.colors.messages.info.fg = gray1
+c.colors.messages.info.bg = green
+c.colors.messages.info.border = green
+c.colors.messages.warning.fg = gray1
+c.colors.messages.warning.bg = yellow
+c.colors.messages.warning.border = yellow
+c.colors.messages.error.fg = luster
+c.colors.messages.error.bg = red
+c.colors.messages.error.border = red
+
+c.colors.prompts.fg = gray7
+c.colors.prompts.bg = gray2
+c.colors.prompts.border = gray4
+c.colors.prompts.selected.fg = luster
+c.colors.prompts.selected.bg = gray3
+
+c.colors.contextmenu.menu.fg = gray7
+c.colors.contextmenu.menu.bg = gray2
+c.colors.contextmenu.selected.fg = luster
+c.colors.contextmenu.selected.bg = gray3
+c.colors.contextmenu.disabled.fg = gray5
+c.colors.contextmenu.disabled.bg = gray2
+
+c.colors.tooltip.fg = gray7
+c.colors.tooltip.bg = gray3
