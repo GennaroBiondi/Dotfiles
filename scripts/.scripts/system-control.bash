04e45653688fa2a53b1d0fd2ce2e5c1351f1be33
GennaroBiondi <throwawayishere@ultrakill.com>
2026-07-28 17:38:06 +0200

    Add Waybar Restart option to System Control

--- scripts/.scripts/system-control.bash
+++ scripts/.scripts/system-control.bash
@@ -6,7 +6,7 @@ if makoctl mode | grep -qx dnd; then
     notification_format="Enable"
 fi
 
-option=$(printf "⏻ Shutdown\n󱥸 Reboot\n󰀄 Logout\n $notification_format notifications" | wofi --dmenu)
+option=$(printf "⏻ Shutdown\n󱥸 Reboot\n󰀄 Logout\n $notification_format Notifications\n Restart Waybar" | wofi --sort-order=default --dmenu)
 if [ "$option" = "⏻ Shutdown" ]; then
     systemctl poweroff
 elif [ "$option" = "󱥸 Reboot" ]; then
@@ -15,4 +15,7 @@ elif [ "$option" = "󰀄 Logout" ]; then
     hyprctl dispatch exit
 elif [ "$option" = " $notification_format notifications" ]; then
     . ~/.local/bin/toggle-mako.sh
+elif [ "$option" = " Restart Waybar" ]; then
+	pkill waybar
+	waybar & disown
 fi
