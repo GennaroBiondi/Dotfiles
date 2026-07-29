e0ef6a76b992f046c45adac28e5b8a7dc03e0fd7
GennaroBiondi <throwawayishere@ultrakill.com>
2026-07-29 16:19:55 +0200

    Make wofi not order entries and fix notifications in sys control menu

--- scripts/.scripts/system-control.bash
+++ scripts/.scripts/system-control.bash
@@ -6,15 +6,15 @@ if makoctl mode | grep -qx dnd; then
     notification_format="Enable"
 fi
 
-option=$(printf "⏻ Shutdown\n󱥸 Reboot\n󰀄 Logout\n $notification_format Notifications\n Restart Waybar" | wofi --sort-order=default --dmenu)
+option=$(printf "⏻ Shutdown\n󱥸 Reboot\n󰀄 Logout\n $notification_format Notifications\n Restart Waybar" | wofi --sort-order=no_sort --dmenu --cache-file /dev/null)
 if [ "$option" = "⏻ Shutdown" ]; then
     systemctl poweroff
 elif [ "$option" = "󱥸 Reboot" ]; then
     systemctl reboot
 elif [ "$option" = "󰀄 Logout" ]; then
     hyprctl dispatch exit
-elif [ "$option" = " $notification_format notifications" ]; then
-    . ~/.local/bin/toggle-mako.sh
+elif [ "$option" = " $notification_format Notifications" ]; then
+	. ~/.scripts/toggle-mako.sh
 elif [ "$option" = " Restart Waybar" ]; then
 	pkill waybar
 	waybar & disown
