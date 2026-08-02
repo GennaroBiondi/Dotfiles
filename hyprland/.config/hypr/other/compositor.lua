32fe2feda8fc6fdc710bcac78c83b7b8823f2565
GennaroBiondi <throwawayishere@ultrakill.com>
2026-08-02 21:57:16 +0200

    change gaps in hyprland windows

--- hyprland/.config/hypr/other/compositor.lua
+++ hyprland/.config/hypr/other/compositor.lua
@@ -1,13 +1,13 @@
 hl.config({
 	general = {
-		gaps_in  = 5,
-		gaps_out = 10,
+		gaps_in          = 5,
+		gaps_out         = 5,
 
-		border_size = 2,
+		border_size      = 2,
 
-		col = {
-			active_border   = "rgba(DEEEEDFF)",
-			inactive_border = "rgba(708090FF)",
+		col              = {
+			active_border   = "rgba(FF0000FF)",
+			inactive_border = "rgba(ff000055)",
 		},
 
 		resize_on_border = false,
